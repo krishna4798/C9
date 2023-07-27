1b8ba49f545814c196f0a7c04250049723e78357
krishna4798 <140745967+krishna4798@users.noreply.github.com>
2023-07-27 18:52:18 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (200,200,30,30)
+
+}
+
+function draw() 
+{
+  background(30);
+if (keyDown("right")){
+  box.position.x +=5
+}
+if (keyDown("left")){
+  box.position.x -=5
+}
+  drawSprites()
+}
+
+
+
+
